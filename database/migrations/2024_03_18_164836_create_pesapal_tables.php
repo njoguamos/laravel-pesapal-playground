0e06b9f98e6d76fb37bc15565552541f856711ec
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2024-03-18 20:01:57 +0300

    Add pint

--- database/migrations/2024_03_18_164836_create_pesapal_tables.php
+++ database/migrations/2024_03_18_164836_create_pesapal_tables.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     public function up(): void
     {
         Schema::create('pesapal_tokens', function (Blueprint $table) {
@@ -15,7 +14,7 @@ return new class extends Migration
             $table->timestamps();
         });
 
-         Schema::create('pesapal_ipns', function (Blueprint $table) {
+        Schema::create('pesapal_ipns', function (Blueprint $table) {
             $table->id();
             $table->string(column: 'url')->unique();
             $table->uuid(column: 'ipn_id');
